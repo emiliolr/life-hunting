1d837160f0d33c468a4480c79caf8c71578980df
emiliolr <emilio.luz.ricca@gmail.com>
2024-04-26 18:02:38 +0100

    Cleaned up model and plot utility functions, cleaned up hurdle model notebook, feel satisfied with benitez-lopez basic replication

--- model_utils.py
+++ model_utils.py
@@ -1,7 +1,12 @@
-import numpy as np
-from sklearn.base import BaseEstimator, RegressorMixin
+import warnings
 
-from utils import get_zero_nonzero_datasets
+import numpy as np
+
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.model_selection import KFold
+from sklearn.metrics import recall_score, balanced_accuracy_score
+
+from utils import get_zero_nonzero_datasets, ratios_to_DI_cats
 
 class PymerModelWrapper:
 
@@ -80,3 +85,86 @@ class HurdleModelEstimator(RegressorMixin, BaseEstimator):
         y_pred = (~y_pred_zero).astype(int) * y_pred_nonzero # if y_pred_zero >= prob_thresh, this is a local extirpation so our prediction should be zero
 
         return y_pred
+
+def k_fold_cross_val(model, data, num_folds = 5, class_metrics = [], reg_metrics = [], verbose = True):
+
+    """
+    A custom k-fold cross validation function that is compatible with a two-stage
+    hurdle model. This will handle transformation of predictions into defaunation
+    categories.
+
+    Paramaters
+    ----------
+    model : sklearn model-like
+
+    data : pandas.DataFrame
+
+    num_folds : integer
+
+    class_metrics : list
+
+    reg_metrics : list
+
+    verbose : boolean
+
+
+    Returns
+    -------
+    metric_dict : dictionary
+
+    """
+
+    kfold = KFold(n_splits = num_folds, random_state = 1693, shuffle = True)
+
+    classes = {0 : 'low', 1 : 'medium', 2 : 'high'}
+    metric_dict = {}
+
+    for m in class_metrics:
+        metric_dict[m] = {classes[c] : [] for c in classes}
+    for m in reg_metrics:
+        metric_dict[m] = []
+
+    for i, (train_idx, test_idx) in enumerate(kfold.split(data)):
+        if verbose:
+            print(f'Fold {i}:')
+        train_data, test_data = data.loc[train_idx].copy(deep = True), data.loc[test_idx].copy(deep = True)
+
+        with warnings.catch_warnings(action = 'ignore'):
+            if verbose:
+                print('  training model')
+            model.fit(train_data)
+            y_pred = model.predict(test_data)
+
+        # Turn predictions into DI categories
+        if verbose:
+            print('  getting test metrics')
+        test_ratios = data['ratio'].loc[test_idx].copy(deep = True)
+        pred_ratios = y_pred.copy()
+        pred_ratios[pred_ratios != 0] = np.exp(pred_ratios[pred_ratios != 0]) # don't need to back-transform predicted extirpations - not RRs!
+
+        pred_DI_cats = ratios_to_DI_cats(pred_ratios)
+        true_DI_cats = ratios_to_DI_cats(test_ratios)
+
+        # Get TEST metrics for this train/test split
+        pseudo_r2 = np.corrcoef(pred_ratios, test_ratios)[0, 1] ** 2
+        if 'pseudo_r2' in reg_metrics:
+            metric_dict['pseudo_r2'].append(pseudo_r2)
+
+        for c in classes:
+            #  binarizing the true/pred labels
+            true = (true_DI_cats == c).astype(int)
+            pred = (pred_DI_cats == c).astype(int)
+
+            #  calculating metrics
+            sens = recall_score(true, pred, pos_label = 1)
+            spec = recall_score(true, pred, pos_label = 0)
+            balanced_acc = balanced_accuracy_score(true, pred)
+
+            if 'sensitivity' in class_metrics:
+                metric_dict['sensitivity'][classes[c]].append(sens)
+            if 'specificity' in class_metrics:
+                metric_dict['specificity'][classes[c]].append(spec)
+            if 'balanced_accuracy' in class_metrics:
+                metric_dict['balanced_accuracy'][classes[c]].append(balanced_acc)
+
+    return metric_dict
